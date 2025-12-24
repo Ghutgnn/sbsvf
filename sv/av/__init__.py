3d1238db2a543af9c94b3b597763d1f9644f9046
sysnycu <sys.cs12@nycu.edu.tw>
2025-12-24 20:37:39 +0800

    refactor: rename autoware adapter

--- sv/av/__init__.py
+++ sv/av/__init__.py
@@ -3,4 +3,4 @@ from .keyboard import KeyboardAV
 from .esmini_embed_AV import EsminiEmbedAV
 
 # from .autoware_gpt import AutowareAV
-from .autoware_gpt import AutowareAV
+from .autoware import AutowareAV
